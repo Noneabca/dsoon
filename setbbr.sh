fdcaab4af9e111f1dd812634ac46189671ab107f
Noneabca <114938393+Noneabca@users.noreply.github.com>
2022-10-06 16:23:02 +0800

    Create setbbr.sh

---
+++ setbbr.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+cd /usr/local/nginx/mytcp
+rm mytcp.conf
+wget https://raw.githubusercontent.com/Noneabca/csoon/main/mytcp.conf
+echo "写入成功"
+nginx -s reload
+echo "热重启成功"
