405fec318d00c2dbe2d5edaaf4da1f4f143cf3d1
Noneabca <114938393+Noneabca@users.noreply.github.com>
2022-10-08 17:32:35 +0800

    Update downweb.sh

--- downweb.sh
+++ downweb.sh
@@ -3,3 +3,5 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 cd /var/www/html
+rm index.html
+wget https://raw.githubusercontent.com/Noneabca/dsoon/main/index.html
