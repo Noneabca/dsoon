e1707509b4e2c6684826202383fbf34cf625f7f8
Noneabca <114938393+Noneabca@users.noreply.github.com>
2022-10-10 00:23:07 +0800

    Update recert.sh

--- recert.sh
+++ recert.sh
@@ -3,5 +3,15 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 cd /usr/local/nginx/mycert
+rm a.crt
+rm a.key
+rm b.crt
+rm b.key
+rm c.crt
+rm c.key
+wget https://raw.githubusercontent.com/Noneabca/csoon/main/mycert/a.crt
+wget https://raw.githubusercontent.com/Noneabca/csoon/main/mycert/a.key
 wget https://raw.githubusercontent.com/Noneabca/csoon/main/mycert/b.crt
 wget https://raw.githubusercontent.com/Noneabca/csoon/main/mycert/b.key
+wget https://raw.githubusercontent.com/Noneabca/csoon/main/mycert/c.crt
+wget https://raw.githubusercontent.com/Noneabca/csoon/main/mycert/c.key
