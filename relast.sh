344cf7ae672b269dbbd2990789f14b36d23fe8ee
Noneabca <114938393+Noneabca@users.noreply.github.com>
2022-11-26 11:10:19 +0800

    Update relast.sh

--- relast.sh
+++ relast.sh
@@ -4,7 +4,9 @@ export PATH
 
 cd /usr/local/nginx/mytcp
 rm mytcp.conf
+rm xitcp.conf
 wget https://raw.githubusercontent.com/Noneabca/csoon/main/mytcp.conf
+wget https://raw.githubusercontent.com/Noneabca/csoon/main/xitcp.conf
 echo "写入成功"
 nginx -s reload
 echo "热重启成功"
