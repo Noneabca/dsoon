62ecaeb3d36b4db666d6c48eed6396fb9c82ac27
Noneabca <114938393+Noneabca@users.noreply.github.com>
2023-03-22 16:59:57 +0800

    Update recert.sh

--- recert.sh
+++ recert.sh
@@ -15,6 +15,10 @@ rm nb.crt
 rm nb.key
 rm nc.crt
 rm nc.key
+rm bna.crt
+rm bna.key
+rm bnb.crt
+rm bnb.key
 wget https://raw.githubusercontent.com/Noneabca/csoon/main/mycert/a.crt
 wget https://raw.githubusercontent.com/Noneabca/csoon/main/mycert/a.key
 wget https://raw.githubusercontent.com/Noneabca/csoon/main/mycert/b.crt
@@ -27,3 +31,7 @@ wget https://raw.githubusercontent.com/Noneabca/csoon/main/mycert/nb.crt
 wget https://raw.githubusercontent.com/Noneabca/csoon/main/mycert/nb.key
 wget https://raw.githubusercontent.com/Noneabca/csoon/main/mycert/nc.crt
 wget https://raw.githubusercontent.com/Noneabca/csoon/main/mycert/nc.key
+wget https://raw.githubusercontent.com/Noneabca/csoon/main/mycert/bna.crt
+wget https://raw.githubusercontent.com/Noneabca/csoon/main/mycert/bna.key
+wget https://raw.githubusercontent.com/Noneabca/csoon/main/mycert/bnb.crt
+wget https://raw.githubusercontent.com/Noneabca/csoon/main/mycert/bnb.key
